ebafb0b137f83697f7650cc8a3856b279c1558e9
Vadim <onlyforprogrammingpost@gmail.com>
2025-04-08 22:29:52 +0500

    create http_bearer for simplicity

--- backend/application/api/handlers/__init__.py
+++ backend/application/api/handlers/__init__.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from core.config import settings
 from .auth import router as auth_router 
 from .user import router as user_router
+from fastapi.security import HTTPBearer
 
-api_router = APIRouter(prefix=settings.prefix.title)
+http_bearer = HTTPBearer(auto_error=False)
+
+api_router = APIRouter(
+  prefix=settings.prefix.title,
+  dependencies=[Depends(http_bearer)]
+)
 api_router.include_router(router = auth_router)
 api_router.include_router(router = user_router)
