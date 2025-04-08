a209dea0d8780a53551f98933b378b7fe4c0f4cf
Vadim <onlyforprogrammingpost@gmail.com>
2025-04-08 18:59:30 +0500

    update auth handler file

--- backend/application/api/handlers/auth.py
+++ backend/application/api/handlers/auth.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
 from core.config import settings
+from api.dependencies.authentication.api_users_router import fastapi_users, authentication_backend
 
 router = APIRouter(
   prefix=settings.prefix.auth,
   tags=["Auth"]
+)
+
+router.include_router(
+  router = fastapi_users.get_auth_router(authentication_backend)
 )
