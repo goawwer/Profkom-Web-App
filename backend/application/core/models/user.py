a84005f87ad828a94a7bf1c1eb47a1ce34fb1b8c
Vadim <onlyforprogrammingpost@gmail.com>
2025-04-08 17:28:11 +0500

    add UserIdType var to user demo model

--- backend/application/core/models/user.py
+++ backend/application/core/models/user.py
@@ -2,11 +2,12 @@ from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeignKey
+from types.user_id import UserIdType
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING: 
   from sqlalchemy.ext.asyncio import AsyncSession
-class User(Base, SQLAlchemyBaseUserTable[int]):
+class User(Base, SQLAlchemyBaseUserTable[UserIdType]):
   
   username: Mapped[str] = mapped_column(String(40), unique=True)
   
