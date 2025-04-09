0f8615371f2fd359de208417f0981fdb4cd740fa
Vadim <onlyforprogrammingpost@gmail.com>
2025-04-09 13:32:14 +0500

    create function for registration where user needs to write group name

--- backend/application/core/authentication/user_manager.py
+++ backend/application/core/authentication/user_manager.py
@@ -1,19 +1,54 @@
 import logging
 from typing import Optional, TYPE_CHECKING
+from fastapi import HTTPException, status
 from fastapi_users import BaseUserManager, IntegerIDMixin
-
+from sqlalchemy import select
+from core.schemas.user import UserCreate, InternalUserCreate
 from core.types.user_id import UserIdType
 from core.models import User
 from core.config import settings
+from core.models import Group
 
 log = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
     from fastapi import Request
+    from sqlalchemy import Result
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
+
+
     reset_password_token_secret = settings.access_token.reset_password_token_secret
     verification_token_secret = settings.access_token.verification_token_secret
+
+    async def create(
+            self, 
+            user_create: UserCreate,  
+            request: Optional["Request"] = None,
+            safe: bool = False
+    ) -> User:
+        group_query = select(Group).where(Group.name == user_create.group_name)
+        result: "Result" = await self.user_db.session.execute(group_query)
+        group = result.scalar_one_or_none()
+
+        if not group:
+            raise HTTPException(
+                status_code = status.HTTP_404_NOT_FOUND,
+                detail = f"Группа с именем {user_create.group_name} не найдена"
+            )
+        
+        user_dict = user_create.model_dump(exclude={"group_name"})
+        user_dict["group_id"] = group.id
+        
+        created_user = await super().create(
+            InternalUserCreate(**user_dict),
+            safe=safe,
+            request=request
+        )
+        
+        created_user.group_name = group.name
+
+        return created_user
 
     async def on_after_register(self, user: User, request: Optional["Request"] = None):
         log.warning("User %r has registered.", user.id)
@@ -27,4 +62,3 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
         self, user: User, token: str, request: Optional["Request"] = None
     ):
         log.warning("Verification requested for user %r. Verification token: %r", user.id, token)
-        
