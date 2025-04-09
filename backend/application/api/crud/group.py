dfcbcab411250e5ea776bc69911947635b375d90
Vadim <onlyforprogrammingpost@gmail.com>
2025-04-09 12:42:18 +0500

    create crud for group

---
+++ backend/application/api/crud/group.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, Result
+from core.models import Group
+from core.schemas import GroupCreate
+
+async def get_all_groups(session: AsyncSession) -> list[Group]:
+  stmt = select(Group).order_by(Group.id)
+  result: Result = await session.execute(stmt)
+  groups = result.scalars().all()
+  return list(groups)
+
+async def get_group_by_id(session: AsyncSession, group_id: int) -> Group:
+  return await session.get(Group, group_id)
+  
+
+async def create_group(session: AsyncSession, group_in: GroupCreate) -> Group:
+  group = Group(**group_in.model_dump())
+  session.add(group)
+  await session.commit()
+  await session.refresh(group)
+  return group 
