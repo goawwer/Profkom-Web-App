5b67d99393307673ba1ada0ece4a9f9978be2920
Vadim <onlyforprogrammingpost@gmail.com>
2025-04-10 17:23:12 +0500

    create endpoints for profkom events

--- backend/application/api/handlers/__init__.py
+++ backend/application/api/handlers/__init__.py
@@ -4,6 +4,7 @@ from .auth import router as auth_router
 from .user import router as user_router
 from .group import router as group_router
 from .plan_item import router as plans_router
+from .profkom_event import router as events_router
 from fastapi.security import HTTPBearer
 
 http_bearer = HTTPBearer(auto_error=False)
@@ -16,3 +17,4 @@ api_router.include_router(router = auth_router)
 api_router.include_router(router = user_router)
 api_router.include_router(router = group_router)
 api_router.include_router(router = plans_router)
+api_router.include_router(router = events_router)
---
+++ backend/application/api/handlers/profkom_event.py
@@ -0,0 +1,133 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from api.crud import profkom_event as crud
+from core.config import settings
+from core.models import User, db_helper, ProfkomEvent
+from core.schemas.profkom_event import ProfkomEventCreate, ProfkomEventUpdate, ProfkomEventUpdatePartial
+from typing import Annotated
+from api.dependencies.authentication.api_users import current_user
+
+router = APIRouter(
+    prefix=settings.prefix.profkom_event,
+    tags=["Profkom events"]
+)
+
+
+@router.get("/all")
+async def get_all_events(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+):
+    return await crud.get_events(session=session)
+
+@router.get("/{event_id}/")
+async def get_event(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    event_id: int
+):
+    return await crud.get_event(
+        session = session,
+        event_id = event_id
+    )
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_event(
+    user: Annotated[User, Depends(current_user)],
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    event_in: ProfkomEventCreate
+):
+    if user.is_superuser:
+        return await crud.create_event(
+            session = session,
+            event_in = event_in,
+            user_id = user.id
+    )
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN
+    )
+
+@router.put("/{event_id}/")
+async def update_event(
+    event_id: int,
+    user: Annotated[User, Depends(current_user)],
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    event_update: ProfkomEventUpdate,
+):
+    if user.is_superuser:
+        event = await crud.get_event(
+        session = session,
+        event_id = event_id,
+        user_id = user.id
+    )
+        if not event:
+            raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"PlanItem с ID {event_id} не найден"
+        )
+        updated_event = await crud.update_event(
+            user_id=user.id,
+            session=session,
+            event = event,
+            event_update=event_update
+        )
+        return updated_event
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN
+    )
+
+
+@router.put("/{event_id}/")
+async def update_event_partial(
+    event_id: int,
+    user: Annotated[User, Depends(current_user)],
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    event_update: ProfkomEventUpdatePartial,
+):
+    if user.is_superuser:
+        event = await crud.get_event(
+        session = session,
+        event_id = event_id,
+        user_id = user.id
+    )
+        if not event:
+            raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"PlanItem с ID {event_id} не найден"
+        )
+        updated_event = await crud.update_event(
+            user_id=user.id,
+            session=session,
+            event = event,
+            event_update=event_update,
+            partial = True
+        )
+        return updated_event
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN
+    )
+
+@router.delete("/{event_id}/")
+async def delete_plan_item(
+    event_id: int,
+    user: Annotated[User, Depends(current_user)],
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    if user.is_superuser:
+        event = await crud.get_event(
+        session = session,
+        event_id = event_id,
+        user_id = user.id
+    )
+        if not event:
+            raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"PlanItem с ID {event_id} не найден"
+        )
+        deleted_event = await crud.delete_event(
+            user_id=user.id,
+            session=session,
+            event = event,
+        )
+        return f"Запись удалена{deleted_event}"
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN
+    )
